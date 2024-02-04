5302989127c05f59667120fd315500bf85d311fe
Mitchell <80652149+HeronErin@users.noreply.github.com>
2024-02-03 19:19:37 -0500

    Update hello.js

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,7 +1,9 @@
 export default async (request, context) => {
   context.log("Calling hello.js edge function");
+  const response = await fetch("https://api.github.com/users/HeronErin");
+	const jresult = await response.text();
 
-  return new Response("Hello, World from the edge!<br>"+Date.now(), {
+  return new Response("Hello, World from the edge!<br>"+Date.now()+"<br>"+jresult, {
     headers: { "content-type": "text/html" },
   });
 };
