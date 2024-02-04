66f06ef8b69ea755d754e07def3d9b09fdae2419
HeronErin <HeronErin@protonmail.com>
2024-02-03 19:28:38 -0500

    Strip down

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,9 +1,9 @@
 export default async (request, context) => {
   context.log("Calling hello.js edge function");
-  const response = await fetch("https://api.github.com/users/HeronErin");
-	const jresult = await response.text();
+  // const response = await fetch("https://api.github.com/users/HeronErin");
+	// const jresult = await response.text();
 
-  return new Response("Hello, World from the edge!<br>"+Date.now()+"<br>"+jresult, {
+  return new Response("Hello, World from the edge!<br>"+Date.now()+"<br>"+"YES", {
     headers: { "content-type": "text/html" },
   });
 };
