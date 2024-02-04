780a8a0c18d45e1a3ade4ea470657625160177cf
Mitchell <80652149+HeronErin@users.noreply.github.com>
2024-02-03 19:15:34 -0500

    Update hello.js

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,7 +1,7 @@
 export default async (request, context) => {
   context.log("Calling hello.js edge function");
 
-  return new Response("Hello, World from the edge!", {
+  return new Response("Hello, World from the edge!<br>"+Date.now(), {
     headers: { "content-type": "text/html" },
   });
 };
