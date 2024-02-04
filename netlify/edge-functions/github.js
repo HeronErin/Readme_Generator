6a08c14cb24a8cccade53d7b603fbe713d17c8a3
HeronErin <HeronErin@protonmail.com>
2024-02-03 19:41:52 -0500

    github function

---
+++ netlify/edge-functions/github.js
@@ -0,0 +1,46 @@
+const api = require("./api_utils.js")
+
+export default async (request, context) => { 
+	const VALID_FEATURES = {issueAndPr: api.issueAndPr, basic: api.basicInfo, repos: api.repoInfo, events: api.events, allCommits: api.allCommits};
+
+	const url = new URL(request.url)
+	const name = url.pathname.slice(1);
+
+
+	// console.log(url.searchParams)
+	let requested_features, ignored;
+	try{
+		requested_features = JSON.parse(url.searchParams.get("features") || "[]");
+		ignored = JSON.parse(url.searchParams.get("ignored") || "[]");
+	}catch(e){
+			return new Response("<h2>Json parse error</h2>", {
+			    headers: { "content-type": "text/html" },
+			    status: 406
+			  });
+	}
+
+	for (let feature of requested_features)
+		if (VALID_FEATURES[feature] == undefined)
+			return new Response("<h2>Unknown feature</h2>", {
+			    headers: { "content-type": "text/html" },
+			    status: 406
+			  });
+	
+	let info = {};
+	let feature_queue = requested_features.map((feature) => VALID_FEATURES[feature](name, ignored));
+	for (future of feature_queue)
+		info = {...info, ...(await future)};
+	return new Response(JSON.stringify(info), {
+	    headers: { "content-type": "text/html" }
+	  });
+
+
+	// fetch("https://api.github.com/search/issues?q=author:HeronErin").then(r=>r.json().then(jso=>{
+	// 	for (let event of jso.items){
+	// 		if (event.pull_request == undefined)
+	// 			console.log(event)
+	// 	}
+	// }))
+
+	// console.log("asd")
+}
